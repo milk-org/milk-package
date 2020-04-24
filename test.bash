55191c1ebb72ca02c72d5cdaf2d588d1c380d118
oguyon <oliv.guyon@gmail.com>
2020-04-24 09:12:05 -1000

    test type in travis

--- test.bash
+++ test.bash
@@ -3,6 +3,9 @@
 if [ $TRAVIS_EVENT_TYPE = "cron" ]; then
 echo "Test  : Nightly"
 ctest -D Nightly
+else if [ $TRAVIS_EVENT_TYPE = "push" ]
+echo "Test: Continuous"
+ctest -D Continuous
 else
 echo "Test: Experimental"
 ctest -D Experimental
