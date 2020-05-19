db720bc07cfc6ad3eff48f2a64fd7e5a54cb8ac7
oguyon <oliv.guyon@gmail.com>
2020-05-18 19:57:55 -1000

    enable MILK_QUIET

--- src/CLImain.c
+++ src/CLImain.c
@@ -43,19 +43,32 @@ int main(
 {
     char *AppName = "milk";
 
-    printf(STYLE_BOLD);
-    printf("\n        Modular Image processing toolKit (milk)\n");
-#ifndef NDEBUG
-    printf("        === DEBUG MODE : assert()         enabled ==========\n");
-    printf("        === DEBUG MODE : DEBUG_TRACEPOINT enabled ==========\n");
-#endif
-    printf(STYLE_NO_BOLD);
 
+    if(getenv("MILK_QUIET"))
+    {
+        data.quiet = 1;
+    }
+    else
+    {
+		data.quiet = 0;
+	}
+
+    if(data.quiet == 0)
+    {
+        printf(STYLE_BOLD);
+        printf("\n        Modular Image processing toolKit (milk)\n");
+#ifndef NDEBUG
+        printf("        === DEBUG MODE : assert()         enabled ==========\n");
+        printf("        === DEBUG MODE : DEBUG_TRACEPOINT enabled ==========\n");
+#endif
+        printf(STYLE_NO_BOLD);
+    }
 
     strcpy(data.package_name, PACKAGE_NAME);
 
     char versionstring[STRINGMAXLEN_VERSIONSTRING];
-    snprintf(versionstring, STRINGMAXLEN_VERSIONSTRING, "%d.%02d.%02d%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_OPTION);
+    snprintf(versionstring, STRINGMAXLEN_VERSIONSTRING, "%d.%02d.%02d%s",
+             VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_OPTION);
 
     data.package_version_major = VERSION_MAJOR;
     data.package_version_minor = VERSION_MINOR;
@@ -67,21 +80,23 @@ int main(
     strcpy(data.configdir, CONFIGDIR);
 
 
-
-    printf("\n");
-    printf("        %s version %s\n", data.package_name, data.package_version);
+    if(data.quiet == 0)
+    {
+        printf("\n");
+        printf("        %s version %s\n", data.package_name, data.package_version);
 #ifdef IMAGESTRUCT_VERSION
-    printf("        Using ImageStreamIO version %s\n", IMAGESTRUCT_VERSION);
+        printf("        Using ImageStreamIO version %s\n", IMAGESTRUCT_VERSION);
 #endif
-    printf("        GNU General Public License v3.0\n");
-    printf("        Report bugs to : %s\n", PACKAGE_BUGREPORT);
-    printf("        Type \"help\" for instructions\n");
-    printf("        \n");
+        printf("        GNU General Public License v3.0\n");
+        printf("        Report bugs to : %s\n", PACKAGE_BUGREPORT);
+        printf("        Type \"help\" for instructions\n");
+        printf("        \n");
+    }
 
     // initialize milk modules for which no function calls is included by default
-	libinit_fft();
+    libinit_fft();
 
-   
+
     libinit_image_basic();
     libinit_img_reduce();
     libinit_linopt_imtools();
