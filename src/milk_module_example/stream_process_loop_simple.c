c16f86b9d3fc82a7e44bf1d7e15c4e238a018005
oguyon <oliv.guyon@gmail.com>
2020-08-24 23:30:38 -1000

    update to example

--- src/milk_module_example/stream_process_loop_simple.c
+++ src/milk_module_example/stream_process_loop_simple.c
@@ -1,6 +1,6 @@
 /**
  * @file    stream_process_loop_simple.c
- * @brief   template for simple stream processing loop 
+ * @brief   template for simple stream processing loop
  *
  *
  */
@@ -19,7 +19,7 @@
 /**
  * @brief simple stream processing
  *
- * Requires that streamA and streamB are already loaded in memory\n
+ * Requires streamA and streamB in memory\n
  *
  *
  */
@@ -35,13 +35,14 @@ errno_t milk_module_example__stream_process_loop_simple(
     imageID streamA_ID = image_ID(streamA_name);
     imageID streamB_ID = image_ID(streamB_name);
 
-	uint64_t NBelem;
-	
-	NBelem = data.image[streamA_ID].md[0].size[0] * data.image[streamA_ID].md[0].size[0];
-	
-	struct timespec *tarray;
-	tarray = (struct timespec*) malloc(sizeof(struct timespec)*loopNBiter);
-	
+    uint64_t NBelem;
+
+    NBelem = data.image[streamA_ID].md[0].size[0] *
+             data.image[streamA_ID].md[0].size[0];
+
+    struct timespec *tarray;
+    tarray = (struct timespec *) malloc(sizeof(struct timespec) * loopNBiter);
+
 
     // check that streams are in memory
     //
@@ -59,14 +60,14 @@ errno_t milk_module_example__stream_process_loop_simple(
 
 
 
-	// run loop
-	// In this simple example, loop waits on streamA to update streamB
-	//
+    // run loop
+    // In this simple example, loop waits on streamA to update streamB
+    //
     for(long iter = 0; iter < loopNBiter; iter++)
-    {		
-		clock_gettime(CLOCK_REALTIME, &tarray[iter]);
-        
-        
+    {
+        clock_gettime(CLOCK_REALTIME, &tarray[iter]);
+
+
         // Wait for semaphore # semtrig of stream A
         // The call will block until semaphore is > 0, and
         // then decrement it and proceed
@@ -78,7 +79,8 @@ errno_t milk_module_example__stream_process_loop_simple(
         data.image[streamB_ID].md[0].write = 1;
 
         // processing and computations can be inserted here to update stream B
-		memcpy(data.image[streamB_ID].array.F, data.image[streamA_ID].array.F, sizeof(float)*NBelem);
+        memcpy(data.image[streamB_ID].array.F, data.image[streamA_ID].array.F,
+               sizeof(float)*NBelem);
 
 
         // increment image counter
@@ -89,35 +91,37 @@ errno_t milk_module_example__stream_process_loop_simple(
         data.image[streamB_ID].md[0].write = 0;
     }
 
-	// timing analysis
-	struct timespec tdiff;
-	
-	// average
-	tdiff = timespec_diff(tarray[0], tarray[loopNBiter-1]);
-	float loopFrequencyHz = 1.0*loopNBiter/(1.0 * tdiff.tv_sec + 1.0e-9 * tdiff.tv_nsec);
-	printf("loopfrequency   : %.3f Hz\n", loopFrequencyHz);
-	printf("Average latency : %9.3f nanosec\n", 1.0e9/loopFrequencyHz/2.0); // divide by two for sinle process latency
+    // timing analysis
+    struct timespec tdiff;
+
+    // average
+    tdiff = timespec_diff(tarray[0], tarray[loopNBiter - 1]);
+    float loopFrequencyHz = 1.0 * loopNBiter / (1.0 * tdiff.tv_sec + 1.0e-9 *
+                            tdiff.tv_nsec);
+    printf("loopfrequency   : %.3f Hz\n", loopFrequencyHz);
+    printf("Average latency : %9.3f nanosec\n",
+           1.0e9 / loopFrequencyHz / 2.0); // divide by two for sinle process latency
 
 
-	int file_output_mode = 0;
-	if( file_output_mode == 1 )
-	{
-	// detailed analysis, print to text
-	float *tdiff_nanosec;
-	FILE *fp;
-	
-	fp = fopen("timinglog.txt", "w");
-	tdiff_nanosec = (float*) malloc(sizeof(float)*(loopNBiter-1));
-	for(long iter=0; iter<loopNBiter-1; iter++)
-	{
-		tdiff = timespec_diff(tarray[iter], tarray[iter+1]);
-		tdiff_nanosec[iter] = 1.0e9 * tdiff.tv_sec + 1.0*tdiff.tv_nsec;
-		fprintf(fp, "%6ld  %6.3f \n", iter, tdiff_nanosec[iter]);
-	}
-	fclose(fp);
-	}
+    int file_output_mode = 0;
+    if(file_output_mode == 1)
+    {
+        // detailed analysis, print to text
+        float *tdiff_nanosec;
+        FILE *fp;
 
-	free(tarray);
+        fp = fopen("timinglog.txt", "w");
+        tdiff_nanosec = (float *) malloc(sizeof(float) * (loopNBiter - 1));
+        for(long iter = 0; iter < loopNBiter - 1; iter++)
+        {
+            tdiff = timespec_diff(tarray[iter], tarray[iter + 1]);
+            tdiff_nanosec[iter] = 1.0e9 * tdiff.tv_sec + 1.0 * tdiff.tv_nsec;
+            fprintf(fp, "%6ld  %6.3f \n", iter, tdiff_nanosec[iter]);
+        }
+        fclose(fp);
+    }
+
+    free(tarray);
 
     return RETURN_SUCCESS;
 }
